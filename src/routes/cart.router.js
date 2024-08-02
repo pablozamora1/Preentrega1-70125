1c59a3ad22c8424aae4cb34ba99fd653c17c0cbe
Pablo Zamora <pablo.zamora@icloud.com>
2024-08-02 19:26:23 -0300

    try catch cart.router

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -6,23 +6,39 @@ const cart = new cartManager();
 
 //agregar carrito
 router.post("/", async (req, res) => {
-  res.send(await cart.addCart());
+  try {
+    res.send(await cart.addCart());
+  } catch (error) {
+    res.send("Error al Agregar el Carrito", error);
+  }
 });
 //leer carrito
 router.get("/", async (req, res) => {
-  res.send(await cart.readCart());
+  try {
+    res.send(await cart.readCart());
+  } catch (error) {
+    res.send("Error al Leer el Carrito", error);
+  }
 });
 
 //buscar carrito por id
 router.get("/:cId", async (req, res) => {
-  const cId = req.params.cId;
-  res.send(await cart.getCartById(cId));
+  try {
+    const cId = req.params.cId;
+    res.send(await cart.getCartById(cId));
+  } catch (error) {
+    res.send("Error al Buscar los Carritos por ID", error);
+  }
 });
 // agregar productos al carrito
 router.post("/:cId/products/:pId", async (req, res) => {
-  const idCart = req.params.cId;
-  const idProduct = req.params.pId;
-  res.send(await cart.addToCart(idCart, idProduct));
+  try {
+    const idCart = req.params.cId;
+    const idProduct = req.params.pId;
+    res.send(await cart.addToCart(idCart, idProduct));
+  } catch (error) {
+    res.send("Error al Agregar productos al Carrito", error);
+  }
 });
 
 export default router;
