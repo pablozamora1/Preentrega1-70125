fa7551744adf73c76bcc90bd48a998463e730121
Pablo Zamora <pablo.zamora@icloud.com>
2024-08-08 01:40:16 -0300

    correcciones en productManager

--- src/controllers/productManager.js
+++ src/controllers/productManager.js
@@ -31,21 +31,19 @@ class ProductManager {
       let { title, description, price, thumbnail, code, stock } = product;
 
       if (!title || !description || !price || !code || !stock) {
-        console.log("Todos los campos son obligatorios");
-        return;
+        return "Todos los campos son obligatorios, por favor completa los campos faltantes";
       }
 
       const readProd = await this.readFiles();
-      
+
       if (readProd.some((item) => item.code === code)) {
-        console.log(`Codigo ${code} Repetido `);
-        return;
+        return `El Codigo ${code} esta Repetido, por favor cambia el codigo del producto y vuelve a intentarlo `;
       }
       product.id = nanoid();
       console.log(product);
       const allProducts = [...readProd, product];
       await this.writeProduct(allProducts);
-      return "Producto Añadido";
+      return product;
     } catch (error) {
       console.log("Error al escribir el archivo", error);
     }
@@ -66,13 +64,14 @@ class ProductManager {
       const find = arrayProd.find((item) => item.id == pid);
 
       if (!find) {
-        console.log("producto no encontrado");
+        return "producto no encontrado";
       } else {
-        console.log("producto encontrado");
-        return find;
+        return "producto encontrado", find;
+        // return find;
       }
     } catch (error) {
-      console.log("Error al leer el archivo", error);
+      console.log(error);
+      return "Error al leer el archivo";
     }
   }
 
@@ -87,7 +86,7 @@ class ProductManager {
       await fs.writeFile(this.path, JSON.stringify(productUpdated, null, 2));
       return "Producto Atualizado";
     } catch (error) {
-      console.log("Error al actualizar el producto", error);
+      return "Error al actualizar el producto", error;
     }
   }
 
